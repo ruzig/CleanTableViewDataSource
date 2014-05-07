8e7584132bba89fad4d0d7e3091d996200e9a664
Phat Le <ltanphat@gmail.com>
2014-05-07 10:12:54 +0700

    Display numbers using CleanTableViewDataSource

--- CleanTableViewDataSourceSample/OANumberListingViewController.m
+++ CleanTableViewDataSourceSample/OANumberListingViewController.m
@@ -7,11 +7,15 @@
 //
 
 #import "OANumberListingViewController.h"
+#import "OAArrayDataSource.h"
+
+NSString *const kNumberCellIdentifier = @"OANumberTableViewCellIdentifier";
 
 @interface OANumberListingViewController ()
 
 @property (strong, nonatomic) UITableView *numberTableView;
 @property (strong, nonatomic) NSArray *numbers;
+@property (strong, nonatomic) OAArrayDataSource *arrayDataSource;
 
 @end
 
@@ -37,9 +41,32 @@
 - (UITableView *)numberTableView
 {
     if (!_numberTableView) {
-        _numberTableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _numberTableView = [[UITableView alloc] initWithFrame:self.view.bounds
+                                                        style:UITableViewStylePlain];
+
+        //To use CleanTableViewDataSource, you should register the cell with identifer
+        [_numberTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kNumberCellIdentifier];
+        _numberTableView.dataSource = self.arrayDataSource;
     }
     return _numberTableView;
+}
+
+- (OAArrayDataSource *)arrayDataSource
+{
+    TableViewCellConfigureBlock configureCell = ^(UITableViewCell *cell, id item) {
+        //Configure cell within this block. If there are many types of cell, write more blocks or functions to configure cell depending on the item passing.
+        cell.textLabel.text = [item stringValue];
+    };
+    IdentifierParserBlock identifierParserBlock = ^NSString *(id item) {
+        //Depend on item data, return the right cell identifier here.
+        return kNumberCellIdentifier;
+    };
+    if (!_arrayDataSource) {
+        _arrayDataSource = [[OAArrayDataSource alloc] initWithItems:self.numbers
+                                              identifierParserBlock:identifierParserBlock
+                                                 configureCellBlock:configureCell];
+    }
+    return _arrayDataSource;
 }
 
 @end
