9c8bd1e2e46ef4e8b917162eaef607b27301ed27
Phat Le <ltanphat@gmail.com>
2014-05-07 09:54:43 +0700

    Initial commit

---
+++ CleanTableViewDataSourceSample/OAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OAAppDelegate.h
+//  CleanTableViewDataSourceSample
+//
+//  Created by Phat, Le Tan on 5/7/14.
+//  Copyright (c) 2014 FourFi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CleanTableViewDataSourceSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CleanTableViewDataSourceSample
+//
+//  Created by Phat, Le Tan on 5/7/14.
+//  Copyright (c) 2014 FourFi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OAAppDelegate class]));
+    }
+}
